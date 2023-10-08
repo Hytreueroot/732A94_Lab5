866c70fa58e929b27769e73d3bf7d5d113f2a257
Sıla Kılıçoğlu <33480898+silakilicoglu@users.noreply.github.com>
2023-10-08 13:53:31 +0200

    test code updated.

--- tests/testthat/test-election.R
+++ tests/testthat/test-election.R
@@ -26,9 +26,9 @@ test_that("participation_rate method works", {
 
 test_that("political_party method works", {
   el_object <- election$new("Malmo")
-  fs <- el_object$participation_rate()
+  fs <- el_object$political_party()
   expect_true(is.data.frame(fs))
-  expect_equal(round(unlist(fs[,4][c(1)]), 4), c(77.2888))
+  expect_equal(round(unlist(fs[,4][c(1)]), 4), c(2.7079))
 })
 
 test_that("first_vote method works", {
